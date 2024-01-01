6b2c64f5303b3ec575e65ae7a3965ac9a568b659
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-01-01 22:13:07 +0300

    Update 20231214140539_InitialCreate.cs

--- HastaneRandevuSistemi/Migrations/20231214140539_InitialCreate.cs
+++ HastaneRandevuSistemi/Migrations/20231214140539_InitialCreate.cs
@@ -10,7 +10,7 @@ namespace HastaneRandevuSistemi.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Doktor",
+                name: "Doktorlar",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
